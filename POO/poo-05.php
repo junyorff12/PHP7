2ab6bf662deea8b209603b76786bee12f8508208
junyorff12 <junyorff12@gmail.com>
2018-08-14 00:24:51 -0300

    emcapsulamento, herança, get_class_method

--- POO/poo-05.php
+++ POO/poo-05.php
@@ -1,2 +1,35 @@
 <?php
 
+class Documento {
+    
+    private $numero;
+
+    public function getNumero()
+    {
+    	return $this->numero;
+
+    }
+
+    public function setNumero($n)
+    {
+    	$this->numero = $n;
+    }
+
+}
+
+class Cpf extends Documento {
+        
+        public function validarCpf($n)
+        {
+        	
+        	echo $this->getNumero()."<br>";
+        	return true;
+        }
+    }
+
+$myDoc = new Cpf();
+$myDoc->setNumero("02989849386");
+
+var_dump($myDoc->validarCpf($myDoc->getNumero()));
+echo "<br>";
+print_r(get_class_methods('Documento'));
